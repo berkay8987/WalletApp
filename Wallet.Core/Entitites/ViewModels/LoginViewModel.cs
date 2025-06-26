78b4592a762cd178e80b5c8da57544fdd5ad4e7c
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-26 11:35:34 +0300

    added remember me

--- Wallet.Core/Entitites/ViewModels/LoginViewModel.cs
+++ Wallet.Core/Entitites/ViewModels/LoginViewModel.cs
@@ -11,8 +11,12 @@ namespace Wallet.Core.Entitites.ViewModels
     {
         [Required(ErrorMessage = "Username must be provided.")]
         public string Username { get; set; }
+
         [Required(ErrorMessage = "Password must be provided.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
     }
 }
