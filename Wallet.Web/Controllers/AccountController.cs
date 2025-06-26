c792bb35d9779b254405dbdb7fa6d49e3ae20e6e
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-26 15:15:58 +0300

    Update AccountController.cs

--- Wallet.Web/Controllers/AccountController.cs
+++ Wallet.Web/Controllers/AccountController.cs
@@ -1,11 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Wallet.Core.Entitites.ViewModels;
 
 namespace Wallet.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AccountController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var client = _httpClientFactory.CreateClient("WalletAPI");
+            var response = await client.PostAsJsonAsync("/api/Auth/login", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var token = await response.Content.ReadAsStringAsync();
+                // HttpContext.Session.SetString("JWToken", token);
+
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
 
