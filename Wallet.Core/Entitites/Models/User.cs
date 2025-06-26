817e174677d974949ba98f907c7227de2b9e361b
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-26 11:23:53 +0300

    Added constraints

--- Wallet.Core/Entitites/Models/User.cs
+++ Wallet.Core/Entitites/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,16 @@ namespace Wallet.Core.Entitites.Models
 {
     public class User : IdentityUser
     {
+        [StringLength(20)]
+        [MaxLength(20)]
+        [Required]
         public string Firstname { get; set; }
+
+        [StringLength(20)]
+        [MaxLength(20)]
+        [Required]
         public string Lastname { get; set; }
+
         public decimal Balance { get; set; }
 
         public ICollection<UserInventory> Inventories { get; set; } = new List<UserInventory>();
