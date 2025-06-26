edfcbb2ea3ef0582ea7a409b023e68c5caa3704f
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-26 15:15:26 +0300

    added AuthController for API

---
+++ Wallet.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Wallet.Core.Entitites.Models;
+using Wallet.Core.Entitites.ViewModels;
+
+namespace Wallet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginViewModel model)
+        {
+            return Ok("1");
+        }
+    }
+}
