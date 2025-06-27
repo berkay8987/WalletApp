0b0322d7cf360354cf5f9575c8be95eaf683b0b5
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-27 11:26:06 +0300

    Seperated custom services for modularity

---
+++ Wallet.Web/Helpers/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wallet.Web.Helpers
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomServices(this IServiceCollection services)
+        {
+            services.AddHttpClient("WalletAPI", client =>
+            {
+                client.BaseAddress = new Uri("https://wallet.api:8081/");
+            }).ConfigurePrimaryHttpMessageHandler(() =>
+                new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                });
+
+            return services;
+        }
+    }
+}
