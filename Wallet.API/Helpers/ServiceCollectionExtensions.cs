92dbfef4defd6476f46732bb8b885df3cff0f08f
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-27 11:26:00 +0300

    Seperated custom services for modularity

---
+++ Wallet.API/Helpers/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Wallet.Core.Entitites.Models;
+using Wallet.DataAccess.Context;
+
+namespace Wallet.API.Helpers
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomServices(this IServiceCollection services,
+                                                            IConfiguration configuration) 
+        {
+
+            var dbHost = configuration.GetValue<String>("DB_HOST");
+            var dbName = configuration.GetValue<String>("DB_NAME");
+            var dbPassword = configuration.GetValue<String>("DB_SA_PASSWORD");
+            var connectionString = $"Server={dbHost},1433;Database={dbName};User Id=sa;Password={dbPassword};TrustServerCertificate=True";
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddIdentity<User, IdentityRole>(options => {
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+            }).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
