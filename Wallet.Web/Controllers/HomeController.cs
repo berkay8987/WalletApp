9d802cea03a19e7ae2687f76480eedfb6091ad3a
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-27 16:49:24 +0300

    Update HomeController.cs

--- Wallet.Web/Controllers/HomeController.cs
+++ Wallet.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Wallet.Web.Models;
 
@@ -18,6 +19,7 @@ namespace Wallet.Web.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
