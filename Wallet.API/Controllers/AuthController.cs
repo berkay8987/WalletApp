5e0fc1f4e762a2a565357c5ad0cd34588b703624
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-27 15:51:38 +0300

    added token generaziton

--- Wallet.API/Controllers/AuthController.cs
+++ Wallet.API/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Wallet.Core.Entitites.Models;
 using Wallet.Core.Entitites.ViewModels;
 
@@ -11,24 +17,37 @@ namespace Wallet.API.Controllers
     {
         private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(SignInManager<User> signInManager, UserManager<User> userManager)
+        public AuthController(SignInManager<User> signInManager, UserManager<User> userManager, IConfiguration configuration)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _configuration = configuration;
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginViewModel model)
         {
-            var result = await _signInManager.PasswordSignInAsync(model.Username!, model.Password!, model.RememberMe, false);
-
-            if (result.Succeeded)
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user == null)
             {
-                return Ok(model);
+                return Unauthorized("User not found");
             }
 
-            return BadRequest(model);
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Password wrong");
+            }
+
+            var jwtToken = GenerateJwtToken(user);
+
+            return Ok(new
+            {
+                token = jwtToken,
+                username = user.UserName
+            });
         }
 
         [HttpPost("register")]
@@ -53,5 +72,26 @@ namespace Wallet.API.Controllers
             return result.Succeeded ? Ok(model) : BadRequest(model);
         }
 
+        private string GenerateJwtToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudiance"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
