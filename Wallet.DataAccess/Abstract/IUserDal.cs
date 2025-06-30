0e77520dbbfcf9f072bb41f7ca87ae3db5f85315
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-30 14:32:50 +0300

    Create IUserDal.cs

---
+++ Wallet.DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet.DataAccess.Abstract
+{
+    public interface IUserDal
+    {
+        public decimal GetBalanceByUserId(string UserId);
+    }
+}
