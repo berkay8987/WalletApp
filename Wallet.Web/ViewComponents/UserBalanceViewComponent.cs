a43879bf5fd051e56c34526a63e8463650959acf
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-30 14:33:02 +0300

    Create UserBalanceViewComponent.cs

---
+++ Wallet.Web/ViewComponents/UserBalanceViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Wallet.DataAccess.Context;
+
+namespace Wallet.Web.ViewComponents
+{
+    public class UserBalanceViewComponent: ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserBalanceViewComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient("WalletAPI");
+            var response = await client.GetAsync("api/User/getbalance");
+
+            if (!response.IsSuccessStatusCode) 
+            {
+                ModelState.AddModelError("", "Failed");
+                return View(0m);
+            }
+
+            var balanceStr = await response.Content.ReadAsStringAsync();
+            decimal balance = decimal.TryParse(balanceStr, out var value) ? value : 0m;
+
+            return View(balance);
+        }
+
+    }
+}
