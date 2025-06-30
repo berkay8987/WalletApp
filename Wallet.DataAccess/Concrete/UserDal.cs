4ab214a13616b849b34d7a1e4ae56ff70e455208
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-30 14:32:53 +0300

    Create UserDal.cs

---
+++ Wallet.DataAccess/Concrete/UserDal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.DataAccess.Abstract;
+using Wallet.DataAccess.Context;
+
+namespace Wallet.DataAccess.Concrete
+{
+    public class UserDal: IUserDal
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserDal(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public decimal GetBalanceByUserId(string UserId)
+        {
+            var user = _context.Users.Find(UserId);
+            return user.Balance;
+        }
+    }
+}
