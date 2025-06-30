653ae6c0990f61ba09af3f0cd96aea22d967569c
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-30 14:33:00 +0300

    Create BalanceController.cs

---
+++ Wallet.Web/Controllers/BalanceController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wallet.Core.Entitites.ViewModels;
+
+namespace Wallet.Web.Controllers
+{
+    public class BalanceController : Controller
+    {
+        public IActionResult Balance()
+        {
+            return View();
+        }
+
+        [HttpPost("UpdateBalance")]
+        public async Task<IActionResult> UpdateBalance(UpdateBalanceViewModel model) 
+        { 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Model not valid.");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
