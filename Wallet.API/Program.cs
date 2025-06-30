f0eee6904a0dabf1dc29e8732e9fe96080a4c6e3
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-30 10:00:33 +0300

    Update Program.cs

--- Wallet.API/Program.cs
+++ Wallet.API/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Wallet.Core.Entitites.Models;
+using Microsoft.OpenApi.Models;
 using Wallet.API.Helpers;
+using Wallet.Core.Entitites.Models;
 using Wallet.DataAccess.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,17 +17,61 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCustomServices(builder.Configuration);
 
+#region Swagger Configuration
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter your token in the format: Bearer {your token}",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+#endregion
+
+#region Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerUrl"];
+    options.Audience = "https://wallet.web:8080";
+    options.RequireHttpsMetadata = false;
+});
+#endregion
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
