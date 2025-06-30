9459e4ef1922ea8eb54d615d96931a58e3831c19
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-30 14:32:57 +0300

    Create UpdateBalanceViewModel.cs

---
+++ Wallet.Core/Entitites/ViewModels/UpdateBalanceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet.Core.Entitites.ViewModels
+{
+    public class UpdateBalanceViewModel
+    {
+        public string Username { get; set; }
+
+        [Range(0, double.MaxValue), Required(ErrorMessage = "Balance must be zero or positive.")]
+        public decimal Balance { get; set; }
+    }
+}
