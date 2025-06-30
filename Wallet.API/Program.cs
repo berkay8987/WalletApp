c44bdb90423ff9b1ce71beed3c7adee95e266650
berkay8987 <mtcilekberkay@hotmail.com>
2025-06-30 10:31:13 +0300

    Update Program.cs

--- Wallet.API/Program.cs
+++ Wallet.API/Program.cs
@@ -56,18 +56,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
 
 app.UseHttpsRedirection();
 
