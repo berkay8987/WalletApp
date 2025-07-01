f2984544ac62bdf83cc64813e89dddceba0ed5c1
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-01 16:15:59 +0300

    Create WalletDashboardController.cs

---
+++ Wallet.Web/Controllers/WalletDashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wallet.Web.Controllers
+{
+    public class WalletDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
