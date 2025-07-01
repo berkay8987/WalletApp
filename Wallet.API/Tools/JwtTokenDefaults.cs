ae1b695225f13ff2e66d5f7ebc6fc8c550b50e43
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-01 09:35:00 +0300

    Create JwtTokenDefaults.cs

---
+++ Wallet.API/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,9 @@
+﻿namespace Wallet.API.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string Secret = "WalletAPPVerySecretKey.8081:Wow!";
+        public const string ValidAudiance = "https://wallet.web:8080";
+        public const string ValidIssuer = "https://wallet.api:8081";
+    }
+}
