893824d6ef1ceead3b1834ebb9f6c050ad37de5a
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-01 10:26:22 +0300

    Update UserBalanceViewComponent.cs

--- Wallet.Web/ViewComponents/UserBalanceViewComponent.cs
+++ Wallet.Web/ViewComponents/UserBalanceViewComponent.cs
@@ -1,23 +1,37 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using System.Security.Claims;
 using Wallet.DataAccess.Context;
+using System.Net.Http.Headers;
 
 namespace Wallet.Web.ViewComponents
 {
     public class UserBalanceViewComponent: ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IDistributedCache _cache;
 
-        public UserBalanceViewComponent(IHttpClientFactory httpClientFactory)
+        public UserBalanceViewComponent(IHttpClientFactory httpClientFactory,
+                                        IDistributedCache cache)
         {
             _httpClientFactory = httpClientFactory;
+            _cache = cache;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var token = await _cache.GetStringAsync("AccessToken");
+
+            if (string.IsNullOrEmpty(token))
+            {
+                return View(-1m);
+            }
+
             var client = _httpClientFactory.CreateClient("WalletAPI");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
             var response = await client.GetAsync("api/User/getBalance");
 
             if (response.IsSuccessStatusCode) 
