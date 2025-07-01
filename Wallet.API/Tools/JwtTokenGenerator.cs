f43f72689e567640fbc3fdc7dc0eb5f3f85b93c4
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-01 09:36:14 +0300

    Update JwtTokenGenerator.cs

--- Wallet.API/Tools/JwtTokenGenerator.cs
+++ Wallet.API/Tools/JwtTokenGenerator.cs
@@ -3,7 +3,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Wallet.Core.Entitites.Models;
-using Wallet.API.Tools;
 
 namespace Wallet.API.Tools
 {
