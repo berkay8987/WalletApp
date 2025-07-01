5b577612ae8b1d12c238c3496ddac021d73150d9
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-01 11:02:01 +0300

    Update BalanceController.cs

--- Wallet.Web/Controllers/BalanceController.cs
+++ Wallet.Web/Controllers/BalanceController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using Wallet.Core.Entitites.ViewModels;
+using System.Net.Http.Headers;
 
 namespace Wallet.Web.Controllers
 {
     [Authorize]
     public class BalanceController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IDistributedCache _cache;
+
+        public BalanceController(IHttpClientFactory httpClientFactory,
+                                 IDistributedCache cache)
+        {
+            _httpClientFactory = httpClientFactory;
+            _cache = cache;
+        }
+
         public IActionResult Balance()
         {
             return View();
@@ -20,6 +32,23 @@ namespace Wallet.Web.Controllers
                 return BadRequest("Model not valid.");
             }
 
+            var token = await _cache.GetStringAsync("AccessToken");
+            if (token == null)
+            {
+                ModelState.AddModelError("", "Failed to update balance.");
+                return View(model);
+            }
+
+            var client = _httpClientFactory.CreateClient("WalletAPI");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.PostAsJsonAsync("api/User/AddBalance", model);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                ModelState.AddModelError("", "Failed to update balance.");
+                return View(model);
+            }
 
             return RedirectToAction("Index", "Home");
         }
