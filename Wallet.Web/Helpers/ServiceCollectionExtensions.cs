e4394a91ca154ab09de731eadaaf5d21ac6f3aab
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-01 10:26:19 +0300

    Update ServiceCollectionExtensions.cs

--- Wallet.Web/Helpers/ServiceCollectionExtensions.cs
+++ Wallet.Web/Helpers/ServiceCollectionExtensions.cs
@@ -1,12 +1,14 @@
-﻿using System.Runtime.CompilerServices;
-using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
 
 namespace Wallet.Web.Helpers
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddCustomServices(this IServiceCollection services)
+        public static IServiceCollection AddCustomServices(this IServiceCollection services,
+                                                           IConfiguration configuration)
         {
             services.AddHttpClient("WalletAPI", client =>
             {
@@ -24,8 +26,11 @@ namespace Wallet.Web.Helpers
                     options.LogoutPath = "/Account/logout";
                 });
 
-
-
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration.GetSection("Redis")["ConnectionString"];
+                options.InstanceName = "WalletAppRedis";
+            });
 
             return services;
         }
