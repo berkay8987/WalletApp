a5c1dd7ceb39ea15b3328205f1f6d1c71e6d755d
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-02 15:02:12 +0300

    Create IProductDal.cs

---
+++ Wallet.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Core.Entitites.Models;
+
+namespace Wallet.DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        bool CreateProduct(Product product);
+
+        List<Product> GetAllProducts();
+
+        bool DeleteProduct(int id);
+    }
+}
