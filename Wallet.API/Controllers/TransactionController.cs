8eada88638c303513130ee85f897e0fe65f4acea
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-02 14:34:38 +0300

    Create TransactionController.cs

---
+++ Wallet.API/Controllers/TransactionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wallet.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : Controller
+    {
+        [HttpPost("CreateTransaction")]
+        public IActionResult CreateTransaction()
+        {
+            return BadRequest("Not implemented");
+        }
+
+        [HttpGet("GetAllTransactions")]
+        public IActionResult GetAllTransactions()
+        {
+            return BadRequest("Not implemented");
+        }
+
+        [HttpPost("UpdateTransactionById")]
+        public IActionResult UpdateTransactionById()
+        {
+            return BadRequest("Not implemented");
+        }
+
+        [HttpPost("DeleteTransactionById")]
+        public IActionResult DeleteTransactionById()
+        {
+            return BadRequest("Not implemented");
+        }
+    }
+}
