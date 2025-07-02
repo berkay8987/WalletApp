26a02b80e2ef4f0f2e5085af0498fc00869fa837
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-02 14:34:40 +0300

    Create ProductController.cs

---
+++ Wallet.API/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wallet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpPost("CreateProduct")]
+        public IActionResult CreateProduct()
+        {
+            return BadRequest("Not implemented");
+        }
+
+        [HttpGet("GetAllProducts")]
+        public IActionResult GetAllProducts()
+        {
+            return BadRequest("Not implemented");
+        }
+
+        [HttpPost("UpdateProductById")]
+        public IActionResult UpdateProductById()
+        {
+            return BadRequest("Not implemented");
+        }
+
+        [HttpPost("DeleteProductById")]
+        public IActionResult DeleteProductById()
+        {
+            return BadRequest("Not implemented");
+        }
+    }
+}
