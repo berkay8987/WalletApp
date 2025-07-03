9db7a5d50b30bc8231b36aa0324a3f05b4d5a21c
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-03 12:13:49 +0300

    Update TransactionController.cs

--- Wallet.API/Controllers/TransactionController.cs
+++ Wallet.API/Controllers/TransactionController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Security.Claims;
+using Wallet.DataAccess.Abstract;
 
 namespace Wallet.API.Controllers
 {
@@ -8,16 +11,29 @@ namespace Wallet.API.Controllers
     [Route("api/[controller]")]
     public class TransactionController : Controller
     {
+        private readonly ITransactionDal _transactionDal;
+
+        public TransactionController(ITransactionDal transactionDal)
+        {
+            _transactionDal = transactionDal;
+        }
+
         [HttpPost("CreateTransaction")]
         public IActionResult CreateTransaction()
         {
             return BadRequest("Not implemented");
         }
 
+        /// <summary>
+        ///     Returns all transactions associated with the logged-in user.
+        /// </summary>
+        /// <returns>List<Transaction></returns>
         [HttpGet("GetAllTransactions")]
         public IActionResult GetAllTransactions()
         {
-            return BadRequest("Not implemented");
+            var userId = User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier).Value;
+            var transactions = _transactionDal.GetAllTransactions(userId);
+            return Ok(transactions);
         }
 
         [HttpPost("UpdateTransactionById")]
