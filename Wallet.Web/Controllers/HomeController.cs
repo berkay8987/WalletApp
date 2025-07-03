6007a10dedb81efca708abe537a3661b877e191d
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-03 12:14:00 +0300

    Update HomeController.cs

--- Wallet.Web/Controllers/HomeController.cs
+++ Wallet.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Wallet.Core.Entitites.Models;
 using Wallet.Web.Models;
 
 namespace Wallet.Web.Controllers
@@ -8,14 +10,30 @@ namespace Wallet.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+                              IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient("WalletAPI");
+            var response = await client.GetAsync("api/Product/GetAllProducts");
+            if (response.IsSuccessStatusCode)
+            {
+                var productsString = await response.Content.ReadAsStringAsync();
+
+                var products = JsonSerializer.Deserialize<List<Product>>(productsString, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return View(products);
+            }
             return View();
         }
 
