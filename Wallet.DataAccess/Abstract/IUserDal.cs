b658d3b231457f497e6009deb3920d4ea33f7f40
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-03 12:14:09 +0300

    Update IUserDal.cs

--- Wallet.DataAccess/Abstract/IUserDal.cs
+++ Wallet.DataAccess/Abstract/IUserDal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wallet.Core.Entitites.Models;
 
 namespace Wallet.DataAccess.Abstract
 {
@@ -13,5 +14,9 @@ namespace Wallet.DataAccess.Abstract
         public decimal AddBalanceByUserId(string userId, decimal value);
 
         public decimal RemoveBalanceByUserId(string userId, decimal value);
+
+        public DateTime GetLastUpdatedByUserId(string userId);
+
+        public User GetUserbyUserId(string userId);
     }
 }
