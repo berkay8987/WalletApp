f89a858b5b3a24a71bd02d917660a34cc69b0634
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-03 12:14:10 +0300

    Create ITransactionDal.cs

---
+++ Wallet.DataAccess/Abstract/ITransactionDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Core.Entitites.Models;
+
+namespace Wallet.DataAccess.Abstract
+{
+    public interface ITransactionDal
+    {
+        List<Transaction> GetAllTransactions(string userId);
+    }
+}
