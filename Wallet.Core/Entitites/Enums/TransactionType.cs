80acb48c6a6d533984e0fc1b313f8cf4d8aea3ba
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-03 16:31:52 +0300

    Create TransactionType.cs

---
+++ Wallet.Core/Entitites/Enums/TransactionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet.Core.Entitites.Enums
+{
+    public enum TransactionType
+    {
+        AddBalance,
+        RemoveBalance,
+        ProductBought
+    }
+}
