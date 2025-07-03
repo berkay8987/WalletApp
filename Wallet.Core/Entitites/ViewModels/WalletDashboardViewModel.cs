41a0b1dd49688a9354cf5296d5023055f349a297
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-03 12:14:12 +0300

    Create WalletDashboardViewModel.cs

---
+++ Wallet.Core/Entitites/ViewModels/WalletDashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Core.Entitites.Models;
+
+namespace Wallet.Core.Entitites.ViewModels
+{
+    public class WalletDashboardViewModel
+    {
+        public User? User { get; set; }
+        public List<Transaction>? Transactions { get; set; }
+    }
+}
