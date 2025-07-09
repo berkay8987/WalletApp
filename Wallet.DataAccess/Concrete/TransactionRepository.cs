14a1567d6b13c69f157b3588bdd3c202f1a321d1
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-09 17:30:36 +0300

    Create TransactionRepository.cs

---
+++ Wallet.DataAccess/Concrete/TransactionRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Core.Entitites.Models;
+using Wallet.DataAccess.Abstract;
+using Wallet.DataAccess.Context;
+
+namespace Wallet.DataAccess.Concrete
+{
+    public class TransactionRepository: ITransactionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Transaction>> GetAllForUserAsync(string userId)
+        {
+            var transactions = await _context.Transactions
+                .Where(u => u.UserId == userId)
+                .ToListAsync();
+                
+            return transactions;
+        }
+
+        public async Task AddAsync(Transaction transaction)
+        {
+            await _context.Transactions.AddAsync(transaction);
+        }
+    }
+}
