2ffaec9e32fa88e1cac3adea27c799dfba3b4a2b
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-09 17:30:31 +0300

    Create ITransactionRepository.cs

---
+++ Wallet.DataAccess/Abstract/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Core.Entitites.Models;
+
+namespace Wallet.DataAccess.Abstract
+{
+    public interface ITransactionRepository
+    {
+        Task<IEnumerable<Transaction>> GetAllForUserAsync(string userId);
+
+        Task AddAsync(Transaction transaction);
+    }
+}
