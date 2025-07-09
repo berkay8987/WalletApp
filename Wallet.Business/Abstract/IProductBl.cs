c999df9ff9346e44bbfe951cc23470823fdcbc44
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-09 14:48:10 +0300

    Create IProductBl.cs

---
+++ Wallet.Business/Abstract/IProductBl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Core.Entitites.Models;
+
+namespace Wallet.Business.Abstract
+{
+    public interface IProductBl
+    {
+        Product UpdateProductPrice(Product product, decimal price);
+    }
+}
