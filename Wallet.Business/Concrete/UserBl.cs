0845354e4480956c7bba32bdfd7e8928a6301b73
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-09 14:48:14 +0300

    Create UserBl.cs

---
+++ Wallet.Business/Concrete/UserBl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Business.Abstract;
+
+namespace Wallet.Business.Concrete
+{
+    public class UserBl: IUserBl
+    {
+
+    }
+}
