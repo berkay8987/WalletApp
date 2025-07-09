c5af64626e10850e554646796df1be09eb3de442
berkay8987 <mtcilekberkay@hotmail.com>
2025-07-09 17:30:21 +0300

    Create ITransactionService.cs

---
+++ Wallet.Business/Abstract/ITransactionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Core.Entitites.Enums;
+using Wallet.Core.Entitites.Models;
+
+namespace Wallet.Business.Abstract
+{
+    public interface ITransactionService
+    {
+        Task<IEnumerable<Transaction>> GetAllForUserAsync(string userId);
+
+        Task AddAsync(string userId, decimal value, TransactionType transactionType);
+    }
+}
